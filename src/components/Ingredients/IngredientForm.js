5d08915202d53255c6f35d1d3202d6eebc0f3e35
Ami Yamanouchi <amiyamanouchi@Amis-MacBook-Pro.local>
2021-03-17 12:51:16 -0700

    finished

---
+++ src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,50 @@
+import React, { memo, useState } from "react";
+
+import Card from "../UI/Card";
+import "./IngredientForm.css";
+
+const IngredientForm = (props) => {
+  // const inputState = useState({ title: "", amount: "" });
+  // const [inputState, setInputState] = useState({ title: "", amount: "" });
+  const [enterTitle, setEnterTitle] = useState("");
+  const [enterAmount, setEnterAmount] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onAddIngredient({ title: enterTitle, amount: enterAmount });
+
+    // ...
+  };
+
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input
+              type="text"
+              id="title"
+              value={enterTitle}
+              onChange={(event) => setEnterTitle(event.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              id="amount"
+              value={enterAmount}
+              onChange={(event) => setEnterAmount(event.target.value)}
+            />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+};
+
+export default memo(IngredientForm);
---
+++ src/components/WeatherApp/WeatherApp.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import CityWeather from "./CityWeather";
+
+const WeatherApp = () => {
+  const [city, setCity] = useState("London");
+  const [cityNameForWeather, setCityNameForWeather] = useState("London");
+
+  const changeCity = (e) => {
+    setCity(e.target.value);
+  };
+
+  const citySearch = (e) => {
+    e.preventDefault();
+    setCityNameForWeather(city);
+  };
+
+  return (
+    <div className="container">
+      <CityWeather cityName={cityNameForWeather} />
+      <div className="row justify-content-center">
+        <form onSubmit={citySearch}>
+          <input type="text" value={city} onChange={changeCity} />
+          <input type="submit" className="btn btn-primary" value="Search!" />
+        </form>
+      </div>
+    </div>
+  );
+};
+export default WeatherApp;
